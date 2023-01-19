41d857bd2b3778e9455f7dd31534601b373f06e0
KAMaz64 <konstantin.a.mazunin@gmail.com>
2023-01-19 03:36:07 +0300

    completed Task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -6,4 +6,32 @@
 
 Console.Clear();
 
+void ShowArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
 
+int[] CreateRandomArray(int length, int start, int end)
+{
+    int[] RandomArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+Console.Write("Введите минимальное значение элемента массива: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите максимальное значение элемента массива: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[] RandomArrayOf8 = CreateRandomArray(8, min, max);
+ShowArray(RandomArrayOf8);
+Console.WriteLine("________________");
