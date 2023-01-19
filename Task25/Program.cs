bddb87139c331d42616eb15ddf276d5d90da825c
KAMaz64 <konstantin.a.mazunin@gmail.com>
2023-01-19 03:08:01 +0300

    completed Task25 and Task27

---
+++ Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+Console.Clear();
+
+int Pow(int Number, int Degree)
+{
+    if(Degree == 0) return 1;
+
+    int Solution = Number;
+    for (int i = 2; i <= Degree; i++)
+    {
+        Solution *= Number;        
+    }
+    return Solution;
+}
+
+Console.Write("Input random integer number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input random positive integer degree: ");
+int D = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{N} to the power of {D} equals {Pow(N,D)}");
---
+++ Task27/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12 */
+
+Console.Clear();
+
+int DigitSum(int Number)
+{
+    int SumOfDigits = 0;
+    int ModNumber = Math.Abs(Number);
+    while(ModNumber > 0)
+    {
+        SumOfDigits += ModNumber % 10;
+        ModNumber /= 10;
+    }
+    return SumOfDigits;
+}
+
+Console.Write("Input random integer number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Sum of digits of {N} equals {DigitSum(N)}");
